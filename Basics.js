b8d2c0b916c9db742b327a456eb7abcb1dfc7b95
Nova-iVex1 <ayansid48@gmail.com>
2026-08-20 17:23:52 +0530

    basic of javascript

---
+++ Basics.js
@@ -0,0 +1,76 @@
+// console.log("Hello, World!");
+// console.log(5 + 4);
+// console.log(5 * 4);
+// console.log("5 + 4");
+// console.log("5 * 4 =" , 5 * 4);
+
+
+
+//                              ------->>>>  Variables  <<<<--------
+// age = 21;
+// console.log(age);
+
+// name = "Ayan";
+// console.log(name);
+
+// console.log(age,name);
+
+
+
+//                          ------>>>>>   var, let, and const   <<<<-------
+// var age = 21;
+// console.log(age);
+// var age = 19; re-declaration is allowed
+// age = 26 Updation is allowed
+// console.log(age);
+
+// let name = "Ayan";
+// console.log(name);
+// let name = "Sid";  re-declaration is not allowed
+// name = "Sid";  Updation is allowed
+// console.log(name);
+
+// const aadharCard = 84394884489;
+// console.log(aadharCard);
+// const aadharCard = 36437843626; re-declaration is not allowed
+// aadharCard = 84394884489; updation is not allowed
+// console.log(aadharCard);
+
+
+
+//                                 ------->>>>  Data Type  <<<<--------
+// let clgName = "xyz university";
+// console.log(clgName);
+// console.log(typeof clgName);
+
+// let mobileNumber = 3948438327;
+// console.log(typeof mobileNumber);
+
+// const doubleQuote = "hyy ayan";
+// const singleQuote = 'hiii ayan';
+// const templateLiteral = `hello ayan
+// sid`;
+// console.log(doubleQuote);
+// console.log(singleQuote);
+// console.log(templateLiteral);
+
+// const userName = "ayan"
+// const userName = "sadiq"
+// const greetingMessage = `hello, ${userName}`;
+// console.log(greetingMessage);
+
+// const userName = "sadiq"
+// const greetingMessage = `hello, ${console.log("nested console.log")}`;
+// console.log(greetingMessage);
+
+// let user;
+// console.log(user);
+// console.log(typeof user);
+
+// let products = null;
+// console.log(products);
+// console.log(typeof products);
+
+// let isAdult = true;
+// console.log(isAdult);
+// console.log(typeof isAdult);

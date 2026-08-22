42a78e108a6b130bb7e6ea653a23ba4a625d7205
Nova-iVex1 <ayansid48@gmail.com>
2026-08-22 20:31:01 +0530

    loop file added

---
+++ Loops.js
@@ -0,0 +1,85 @@
+// for Loop  ------->
+// SYNTAX ---->
+// for (initialization; condition; updation) {
+    // Code to execute on each iteration
+// }
+
+// EG -->
+// for (let index = 1; index < 11; index++) {
+//     console.log(index);
+// }
+
+// let index = 1
+// for (; index <= 5 ;) {
+//     console.log(index);
+//     index++;
+// }
+
+// let num = 100;
+// for (let i = 1; i <= num; i++) {
+//     console.log(i);
+// }
+
+
+
+// while Loop  ----->>
+// SYNTAX ---->
+// initialization;
+// while ( condition ){
+    // code block to be executed
+    // updater (e.g., increment or decrement)
+// }
+
+// EG ->
+// let i = 1 ;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// do-while Loop ------>
+// SYNTAX ---->
+// do {
+    // Code block to be executed
+    // updater (e.g., increment or decrement)
+// } while (condition);
+
+// EG ->
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 5);
+
+
+
+// break ---->
+// EG ->
+// for(let i = 1; i <= 5; i++){
+//     if(i === 3) break;
+//     console.log(i);
+// }
+
+
+
+// continue  ----->
+// EG ->
+// for(let i = 1; i <= 5; i++){
+//     if(i === 3) continue;
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     if(i % 3 === 0) continue;
+//     console.log(i);
+// }
+
+
+
+// Table of 5 
+// let num = 7
+// for(let i=1; i<=10; i++){
+//     console.log(num, "*", i, "=", num*i);
+// }

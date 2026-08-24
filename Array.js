327ae85f06784429a24a4b4f5d562c7f14c44433
Nova-iVex1 <ayansid48@gmail.com>
2026-08-24 20:04:10 +0530

    array file added

---
+++ Array.js
@@ -0,0 +1,53 @@
+// an array is a global object used to store ordered collections of data. They are zero-indexed, dynamically resizable, and can hold a mixture of different data types simultaneously
+
+// let Products = ["t-shirt" , "lower" , "cap" , "shoes"];
+
+// console.log(Products);
+
+// console.log(Products[0]);
+// console.log(Products[1]);
+// console.log(Products[2]);
+// console.log(Products[3]);
+
+// console.log(Products[4]);  -> undefined
+
+// console.log(Products.length);
+
+// let str  = "Ayan Sid";
+// console.log(str.length);
+
+// let n = Products.length;
+// console.log(Products[n-1]);
+
+// console.log(Products[Products.length-1]);
+
+// console.log(Products.at(-1));
+
+// console.log(Products.at(-4));
+
+// for(let i=0; i<n; i++){
+//     console.log(Products[i]);
+// }
+
+
+
+// let products = [["t-shirt", 569], ["lower", 459], ["cap", 199], ["shoes", 1500]];
+
+// for(let i=0; i<products.length; i++){
+//     for(let j=0; j<products[0].length; j++){
+//         process.stdout.write(products[i][j] + " ");
+//     }
+//     console.log(" ");
+// }
+
+// let Products = ["t-shirt" , "lower" , "cap" , "shoes"];
+
+// Products.push("shirt")
+
+// Products.pop()
+
+// Products.unshift("shirt")
+
+// Products.shift()
+
+// console.log(Products);
